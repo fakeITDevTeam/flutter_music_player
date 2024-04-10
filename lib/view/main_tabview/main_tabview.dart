7a20f04dc6686f308289366d38b4c027e3017384
fakeITDevTeam <dev5544455@gmail.com>
2024-04-10 15:15:09 +0700

    create splash view and main tabview with bottom navbar

---
+++ lib/view/main_tabview/main_tabview.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:music_player/common/color_extension.dart';
+
+class MainTabView extends StatefulWidget {
+  const MainTabView({super.key});
+
+  @override
+  State<MainTabView> createState() => _MainTabViewState();
+}
+
+class _MainTabViewState extends State<MainTabView> with SingleTickerProviderStateMixin {
+
+  TabController? controller;
+  int selectTab = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = TabController(length: 3, vsync: this);
+
+    controller?.addListener(() {
+      selectTab = controller?.index ?? 0;
+      setState(() {
+        
+      });
+    });
+  }
+
+  @override
+  void dispose() {    
+    super.dispose();
+    controller?.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: TabBarView(
+        controller: controller,
+        children: const [
+          // Container(child: Center(child: Text("Home")),),
+          Center(child: Text("Home")),
+          Center(child: Text("Songs")),
+          Center(child: Text("Settings")),
+          // Container(child: Center(child: Text("Songs")),),
+          // Container(child: Center(child: Text("Settings")),),
+        ],
+      ),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          color: TColor.bg,
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.black38,
+              blurRadius: 5,
+              offset: Offset(0, -3),
+            ),
+          ],
+        ),
+        child: BottomAppBar(
+          color: Colors.transparent,
+          elevation: 0,
+          child: TabBar(
+            controller: controller,
+            indicatorColor: Colors.transparent,
+            indicatorWeight: 1,
+            labelColor: TColor.primary,
+            labelStyle: const TextStyle(
+              fontSize: 10,
+            ),
+            unselectedLabelColor: TColor.primaryText28,
+            unselectedLabelStyle: const TextStyle(fontSize: 10),
+            tabs: [
+              Tab(
+                text: "Home", 
+                icon: Image.asset(
+                  selectTab == 0 ? "assets/img/home_tab.png" : "assets/img/home_tab_un.png", 
+                  width: 20, 
+                  height: 20,
+                ),
+              ),
+              Tab(
+                text: "Songs", 
+                icon: Image.asset(
+                  selectTab == 1 ? "assets/img/songs_tab.png" : "assets/img/songs_tab_un.png", 
+                  width: 20, 
+                  height: 20,
+                ),
+              ),
+              Tab(
+                text: "Settings", 
+                icon: Image.asset(
+                  selectTab == 2 ? "assets/img/setting_tab.png" : "assets/img/setting_tab_un.png", 
+                  width: 20, 
+                  height: 20,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
